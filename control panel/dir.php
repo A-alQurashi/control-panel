be18f25002d4432803ab5c242e17f451bcae68d6
A-alQurashi <86428034+A-alQurashi@users.noreply.github.com>
2021-07-02 17:41:14 +0300

    Add files via upload

---
+++ control panel/dir.php
@@ -0,0 +1,15 @@
+<?php
+   $host= 'localhost';
+   $user= 'root';
+   $pass= '';
+   $db= 'sliders';
+   $con = mysqli_connect("localhost", "root", "", "sliders");
+if (!$con)
+{
+  die("connection error");
+}
+$urltex= "value range for armes";
+$reqs ="SELECT * FROM direction ;";
+$query="INSERT INTO direction Forward) VALUES ('Forward');";
+echo ""
+?>
---
+++ control panel/direction.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2021 at 10:53 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sliders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `direction`
+--
+
+CREATE TABLE `direction` (
+  `Forward` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `left` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `right` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `backward` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `Stop` varchar(8) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `direction`
+--
+
+INSERT INTO `direction` (`Forward`, `left`, `right`, `backward`, `Stop`) VALUES
+('', '', '', '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
